bf1430322e88ab6b168981e5d1d1d8ed406ec0a2
TomokiNakamaru <tomoki.nakamaru@gmail.com>
2016-04-30 12:27:21 +0900

    testing with 3 architecture

--- nakamaru/02/mlp-1.py
+++ nakamaru/02/mlp-1.py
@@ -11,7 +11,7 @@ from keras.utils import np_utils
 
 batch_size = 32
 nb_classes = 10
-nb_epoch = 10
+nb_epoch = 50
 data_augmentation = True
 
 # input image dimensions
@@ -28,12 +28,13 @@ Y_test = np_utils.to_categorical(y_test, nb_classes)
 
 model = Sequential()
 
-model.add(Convolution2D(128, 4, 4, input_shape=(3, 32, 32)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten(input_shape=(3, 32, 32)))
 
-model.add(Flatten())
+for _ in range(3):
+    model.add(Dense(100, bias_bplus=True))
+    model.add(Activation('relu'))
 
+model.add(Dropout(0.5))
 model.add(Dense(nb_classes))
 model.add(Activation('softmax'))
 
@@ -57,16 +58,17 @@ else:
     print('Using real-time data augmentation.')
 
     # this will do preprocessing and realtime data augmentation
-    datagen = ImageDataGenerator(featurewise_center=False,
-                                 samplewise_center=False,
-                                 featurewise_std_normalization=False,
-                                 samplewise_std_normalization=False,
-                                 zca_whitening=False,
-                                 rotation_range=0,
-                                 width_shift_range=0.1,
-                                 height_shift_range=0.1,
-                                 horizontal_flip=True,
-                                 vertical_flip=False)
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
 
     # compute quantities required for featurewise normalization
     # (std, mean, and principal components if ZCA whitening is applied)
--- nakamaru/02/ref1-modified.py
+++ nakamaru/02/ref1-modified.py
@@ -1,4 +1,15 @@
-# coding:utf-8
+'''Train a simple deep CNN on the CIFAR10 small images dataset.
+
+GPU run command:
+    THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python cifar10_cnn.py
+
+It gets down to 0.65 test logloss in 25 epochs, and down to 0.55 after 50 epochs.
+(it's still underfitting at that point, though).
+
+Note: the data was pickled with Python 2, and some encoding issues might prevent you
+from loading it in Python 3. You might have to load it in Python 2,
+save it in a different format, load it in Python 3 and repickle it.
+'''
 
 from __future__ import print_function
 from keras.datasets import cifar10
@@ -11,7 +22,7 @@ from keras.utils import np_utils
 
 batch_size = 32
 nb_classes = 10
-nb_epoch = 10
+nb_epoch = 25
 data_augmentation = True
 
 # input image dimensions
@@ -21,6 +32,9 @@ img_channels = 3
 
 # the data, shuffled and split between train and test sets
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
 
 # convert class vectors to binary class matrices
 Y_train = np_utils.to_categorical(y_train, nb_classes)
@@ -28,17 +42,26 @@ Y_test = np_utils.to_categorical(y_test, nb_classes)
 
 model = Sequential()
 
-model.add(Convolution2D(128, 4, 4, input_shape=(3, 32, 32)))
+model.add(Convolution2D(32, 3, 3, border_mode='same',
+                        input_shape=(img_channels, img_rows, img_cols)))
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+
+model.add(Convolution2D(64, 3, 3, border_mode='same'))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
 model.add(Flatten())
-
+model.add(Dense(128))
+model.add(Activation('relu'))
 model.add(Dense(nb_classes))
 model.add(Activation('softmax'))
 
+# let's train the model using SGD + momentum (how original).
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy',
-              optimizer='adagrad',
+              optimizer=sgd,
               metrics=['accuracy'])
 
 X_train = X_train.astype('float32')
@@ -57,16 +80,17 @@ else:
     print('Using real-time data augmentation.')
 
     # this will do preprocessing and realtime data augmentation
-    datagen = ImageDataGenerator(featurewise_center=False,
-                                 samplewise_center=False,
-                                 featurewise_std_normalization=False,
-                                 samplewise_std_normalization=False,
-                                 zca_whitening=False,
-                                 rotation_range=0,
-                                 width_shift_range=0.1,
-                                 height_shift_range=0.1,
-                                 horizontal_flip=True,
-                                 vertical_flip=False)
+    datagen = ImageDataGenerator(
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
 
     # compute quantities required for featurewise normalization
     # (std, mean, and principal components if ZCA whitening is applied)
